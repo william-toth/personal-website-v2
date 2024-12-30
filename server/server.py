b0bf465fc8d4dc5b3ca1338d83529b7a4c736dc5
william-toth <wat4th@gmail.com>
2024-12-30 09:15:59 -0800

    move repo around

---
+++ server/server.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify, request
+from pymongo import MongoClient
+import os
+
+app = Flask(__name__) 
+
+connection_string = os.environ.get('CONNECTION_STRING')
+
+client = MongoClient(connection_string)
+
+@app.route('/', methods = ['POST']) 
+def home():
+    try:
+        data = request.get_json() 
+        client.guestbook.Collection.insert_one(data)
+        return jsonify({'message': 'Guest book entry logged successfully'}), 200
+    except:
+        return jsonify({'message': 'An error occured logging guest book entry'}), 500
+    
+if __name__ == '__main__': 
+  
+    app.run(debug = True) 
