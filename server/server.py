fd5fcbc2daae0b95aa8292001f12c5b1c6236a5b
william-toth <wat4th@gmail.com>
2024-12-30 12:58:49 -0800

    add

--- server/server.py
+++ server/server.py
@@ -1,25 +1,22 @@
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
 import os
+import ssl
 
 app = Flask(__name__) 
 
 connection_string = os.environ.get('CONNECTION_STRING')
 
-client = MongoClient(connection_string)
+client = MongoClient('mongodb+srv://wat4th:FreshNed123@willcluster.s90is.mongodb.net/', ssl_cert_reqs=ssl.CERT_NONE)
 
 @app.route('/', methods = ['POST']) 
 def home():
-    print(request.get_json())
-    # try:
-    #     data = request.get_json() 
-    #     client.guestbook.Collection.insert_one(data)
-    #     return jsonify({'message': 'Guest book entry logged successfully'}), 200
-    # except:
-    #     return jsonify({'message': 'An error occured logging guest book entry'}), 500
-    data = request.get_json() 
-    client.guestbook.Collection.insert_one(data)
-    return jsonify({'message': 'Guest book entry logged successfully'}), 200
+    try:
+        data = request.get_json() 
+        client.guestbook.Collection.insert_one(data)
+        return jsonify({'message': 'Guest book entry logged successfully'}), 200
+    except:
+        return jsonify({'message': 'An error occured logging guest book entry'}), 500
     
 if __name__ == '__main__': 
   
